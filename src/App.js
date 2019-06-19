eb5a85c94b658f907facaa92fa1652692f7b9b27
SeanergyMacPro006 <seanergylap006@SeanergyLap006s-MacBook-Pro.local>
2019-06-19 18:38:38 +0530

    https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car\?format\=json api  response to be shown in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class UnsignedOrders extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { data: [] }
+	}
+	
+	loadData() {
+		fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
+			.then(response => response.json())
+			.then(data => {
+        this.setState({data: data["Results"] })
+        
+        console.log(typeof data["Results"])
+		})
+			.catch(err => console.error(this.props.url, err.toString()))
+	}
+	
+	signOrder(id) {
+		let ids = [
+		{ 'orderId': id }
+		];
+		console.log(ids)
+		fetch('example.com/api/orders/unsigned', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(ids)
+		})
+		
+	}
+	
+	componentDidMount() {
+		this.loadData()
+	}
+	
+  render() {
+    return <ul>
+			<li className='title'>
+				<span>Make_Id</span>
+				<span>Make_Name</span>
+				<span>VehicleType_Id</span>
+				<span>VehicleType_Name</span>
+			</li>
+      { this.state.data.map((item) => {
+				let statusClass = 'sign'
+				return <li className='item'>
+					<span>{item.MakeId}</span>
+					<span>{item.MakeName}</span>
+					<span>{item.VehicleTypeId}</span>
+					<span></span>
+				</li>
+        })
+      }
+    </ul>;
+  }
 }
+			
 
-export default App;
+
